712df35cca5afc114515e96de1f3383cbef9466e
atsushimemet <noap3b69n@gmail.com>
2022-02-11 12:10:22 +0900

    Fix: black

--- model.py
+++ model.py
@@ -9,7 +9,8 @@ class Model:
     Args:
         datamart: the stock datamart you want to predict
         feature: explanatory variables
-    """    
+    """
+
     def __init__(self, datamart, feature):
         self._datamart = datamart
         self._feature = feature
@@ -17,7 +18,7 @@ class Model:
     def fit(self):
         self.clf = lgb.LGBMClassifier()
         self.df = pd.concat([self._datamart, self._feature], axis=1)
-        self.X = self.df.iloc[:,1:]
+        self.X = self.df.iloc[:, 1:]
         self.y = self.df["target"]
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
             self.X, self.y, test_size=0.3, random_state=0
